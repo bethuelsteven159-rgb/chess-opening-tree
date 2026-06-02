bc8d5adea8b381980cbb5a19cda6a7f5efd23930
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-06-02 06:04:26 +0200

    Update login.js

--- js/auth/login.js
+++ js/auth/login.js
@@ -6,7 +6,7 @@ googleLoginBtn.addEventListener("click", async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin + "/random.html"
+      redirectTo: "https://bethuelsteven159-rgb.github.io/chess-opening-tree/random.html"
     }
   });
 
