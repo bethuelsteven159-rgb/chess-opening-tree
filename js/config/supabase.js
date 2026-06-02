463703390bd5dd0e37548289dc7a5a363d8bcf03
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-06-02 05:11:37 +0200

    Add Supabase client configuration

---
+++ js/config/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://puhscovkftoffykeyzze.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aHNjb3ZrZnRvZmZ5a2V5enplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNDI3NDEsImV4cCI6MjA5NTkxODc0MX0.N1vUdm7UhPxc9KqGRIutgOEqy8PBTyv529rsA4uGwCE"
+);
